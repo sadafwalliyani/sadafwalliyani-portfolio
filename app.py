3bdf8261d496b12091bfe6ebe5e53c37484464c1
sadafwalliyani <sadafwalliyani@gmail.com>
2024-08-22 08:18:25 +0500

    done

--- app.py
+++ app.py
@@ -6,22 +6,27 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 
 # Set up Jinja2 templates
-templates = Jinja2Templates(directory=r"D:\\sadafwalliyani-portfolio\\templates")
+templates = Jinja2Templates(directory=r'D:\sadafwalliyani-portfolio\templates')
 
 
-# Route for the Home page
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
+    print("Home page accessed")  # Add this line
     return templates.TemplateResponse("home.html", {"request": request})
 
-# Route for the About Us page
-@app.get("/about", response_class=HTMLResponse)
-def about(request: Request):
+@app.get("/AI_models", response_class=HTMLResponse)
+def ai_models(request: Request):
+    print("AI Models page accessed")  # Add this line
+    return templates.TemplateResponse("AI_models.html", {"request": request})
+
+@app.get("/about_us", response_class=HTMLResponse)
+def about_us(request: Request):
+    print("About Us page accessed")  # Add this line
     return templates.TemplateResponse("about_us.html", {"request": request})
 
-# Route for the Contact Us page
-@app.get("/contact", response_class=HTMLResponse)
-def contact(request: Request):
+@app.get("/contact_us", response_class=HTMLResponse)
+def contact_us(request: Request):
+    print("Contact Us page accessed")  # Add this line
     return templates.TemplateResponse("contact_us.html", {"request": request})
 
 # Route for generating text or any other functionality you want to add later
